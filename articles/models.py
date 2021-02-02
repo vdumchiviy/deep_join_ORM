056957b1d2c9a37dc8b4c33bcf8307017d67e3de
Maksim Kovtun <brain1999@mail.ru>
2021-02-02 15:24:16 +0300

    Added class AricleScope, translated to English, some minore files

--- articles/models.py
+++ articles/models.py
@@ -3,14 +3,28 @@ from django.db import models
 
 class Article(models.Model):
 
-    title = models.CharField(max_length=256, verbose_name='Название')
-    text = models.TextField(verbose_name='Текст')
-    published_at = models.DateTimeField(verbose_name='Дата публикации')
-    image = models.ImageField(null=True, blank=True, verbose_name='Изображение',)
+    title = models.CharField(max_length=256, verbose_name='Name')
+    text = models.TextField(verbose_name='Content')
+    published_at = models.DateTimeField(verbose_name='Release Date')
+    image = models.ImageField(null=True, blank=True,
+                              verbose_name='Image',)
 
     class Meta:
-        verbose_name = 'Статья'
-        verbose_name_plural = 'Статьи'
+        verbose_name = 'Article'
+        verbose_name_plural = 'Articles'
 
     def __str__(self):
         return self.title
+
+
+class ArticleScope(models.Model):
+
+    name = models.CharField(max_length=50, verbose_name="Name")
+    members = models.ManyToManyField(Article, verbose_name="Member")
+
+    class Meta:
+        verbose_name = "Scope"
+        verbose_name_plural = "Scopes"
+
+    def __str__(self):
+        return self.name
