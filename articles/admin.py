57a2b3c4edc4da243ff8985f8e64663b50cbbae3
Maksim Kovtun <brain1999@mail.ru>
2021-02-02 16:11:20 +0300

    Added class ArticleScope to admin

--- articles/admin.py
+++ articles/admin.py
@@ -1,8 +1,15 @@
 from django.contrib import admin
 
-from .models import Article
+from .models import Article, ArticleScope
+
+# superuser for admin: admin/1
 
 
 @admin.register(Article)
 class ArticleAdmin(admin.ModelAdmin):
+    pass
+
+
+@admin.register(ArticleScope)
+class ArticleScopeAdmin(admin.ModelAdmin):
     pass
