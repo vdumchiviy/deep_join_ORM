3f2172c9545efd59e591721bdaeef1e128e6232d
Maksim Kovtun <brain1999@mail.ru>
2021-02-12 16:03:08 +0300

    Solving problems with scope.is_favorite

--- articles/views.py
+++ articles/views.py
@@ -6,11 +6,10 @@ from articles.models import Article, Scope, ArticleScope
 
 def articles_list(request):
     template = 'articles/news.html'
-    articles_list = Article.objects.all()
-    context = {}
-
+    articles = Article.objects.all().prefetch_related('scopes')
     # используйте этот параметр для упорядочивания результатов
     # https://docs.djangoproject.com/en/2.2/ref/models/querysets/#django.db.models.query.QuerySet.order_by
     ordering = '-published_at'
-    context['object_list'] = ['1', '2', '3']
+    context = {'object_list': articles}
+
     return render(request, template, context)
