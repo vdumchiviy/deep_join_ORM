28c0bb44980493c90286a6ead21680511c61a9cc
Maksim Kovtun <brain1999@mail.ru>
2021-02-14 23:38:29 +0300

    Changed to new branch

--- articles/views.py
+++ articles/views.py
@@ -1,14 +1,16 @@
 from django.views.generic import ListView
 from django.shortcuts import render
-
+from django.db.models import OuterRef, Subquery, Prefetch
 from articles.models import Article, Scope, ArticleScope
 
 
 def articles_list(request):
     template = 'articles/news.html'
     ordering = '-published_at'
-    articles = Article.objects.prefetch_related(
-        'scope_relations').all().order_by(ordering)
+    articles = Article.objects.all().prefetch_related(
+        'scope_relations').order_by(ordering)
+    # articles = Article.objects.all().prefetch_related(
+    #     'scope_relations').order_by(ordering)
     # используйте этот параметр для упорядочивания результатов
     # https://docs.djangoproject.com/en/2.2/ref/models/querysets/#django.db.models.query.QuerySet.order_by
     context = {'object_list': articles}
